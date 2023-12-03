7372ed9bd16b275b316d58dd7272f65c1383d846
naveenkumar061 <96903304+naveenkumar061@users.noreply.github.com>
2023-12-03 08:45:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,35 @@
-import React, { useState } from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
+
 const App = () => {
-
-  const [selectShape,setselectShape]=useState("square");
-  const [shapes,setShapes]=useState([]);
-
-  const optionValue=(event)=>{
+  const [selectShape, setselectShape] = useState("square");
+  const [shapes, setShapes] = useState([]);
+  const optionValue = (event) => {
     setselectShape(event.target.value);
-  }
-
-  const buttonClick=()=>{
+  };
+  const buttonClick = () => {
     const newShape = {
-      id:shapes.length+1,
-      type:selectShape,
-    }
-
-    setShapes([...shapes,newShape]);
-  }
-
+      id: shapes.length + 1,
+      type: selectShape,
+    };
+    setShapes([...shapes, newShape]);
+  };
   return (
     <div id="main">
-
       <div id="shape-creator">
         <select value={selectShape} onChange={optionValue}>
           <option value="square">Square</option>
           <option value="circle">Circle</option>
         </select>
-        <button onClick={buttonClick}>Add Shape</button>        
-
+        <button onClick={buttonClick}>Add Shape</button>
       </div>
       <div id="shapes-holder">
-        {shapes.map((shape)=>(
-          <div className={shape.type}>{${shape.id}}</div>
+        {shapes.map((shape) => (
+          <div className={shape.type}>{shape.id}</div>
         ))}
-    
       </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
